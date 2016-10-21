c1f2e2f0fbfc708a64ac3e3727406b0eb0f906f1
mberlanda <mauroberlanda@gmail.com>
2016-10-21 22:07:15 +0200

    rename application

--- DEV204x/Properties/AssemblyInfo.cs
+++ DEV204x/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("DEV204x")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("DEV204x")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe82f04b-19a9-42e7-aa69-e7d4165ef0f9")]
+[assembly: Guid("0ee14a03-91e4-4225-b6a1-7d35641973fa")]
 
 // Version information for an assembly consists of the following four values:
 //
